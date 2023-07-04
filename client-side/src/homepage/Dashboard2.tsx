1b10665a9361cb262339fb112a85e0837c3c4f22
laysophann <lay.sophann19@kit.edu.kh>
2023-07-04 20:28:13 +0700

    already fixed responsiveness

--- client-side/src/homepage/Dashboard2.tsx
+++ client-side/src/homepage/Dashboard2.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-
 import CreatePoll from "./responsive/app-community/CreatePoll";
 import Community from "./responsive/app/Community";
 import CommunityProfile from "./responsive/app-community-detail/CommunityProfile";
 
 function Dashboard2() {
   return (
-    <div className="flex flex-row bg-slate-200 w-screen">
+    <div className="flex flex-row bg-white w-screen">
       <Community />
       <CreatePoll />
       <CommunityProfile />
