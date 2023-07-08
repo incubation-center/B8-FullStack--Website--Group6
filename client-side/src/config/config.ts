767cc1d1c710cec2bd9fa3b59e65c7419f575787
lysreypov <ly.sreypov19@kit.edu.kh>
2023-07-08 11:45:08 +0700

    update config file

--- client-side/src/config/config.ts
+++ client-side/src/config/config.ts
@@ -2,6 +2,6 @@
 
 export const apiURL = "http://52.220.220.40:8080";
 export const accessToken =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5YW1hIiwiZXhwIjoxNjg4ODA5Mzk0fQ._ZrL27lQRjFjzsewlKhjbTJUoFMxSroTVkE9Zp-NFQVf0xRJvz0iCIDFBWy85dbI0PopH_pUQveCpGQvB49M7g";
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwb3YiLCJleHAiOjE2ODg4MTEwNTF9.PAGHz_uK0yQzOvDQc2m9ALCjyQVupNSZVe2YnVfmh2oWCWco60ckcVrIgoQzf31rPOQKdySYrUgmYyF850XiEQ";
 export const clientId =
   "169663001832-rnhhvl1ump4dj98k56gi44ejt8h5i0mn.apps.googleusercontent.com";
